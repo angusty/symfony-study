c9e157d4eeebde686a1a6417279510b2cbe1f60e
yboker <8236138@qq.com>
2015-03-05 17:37:40 +0800

    用DoctrineFixturesBundle生成数据

---
+++ src/Ibw/JobeetBundle/DataFixtures/ORM/LoadCategoryData.php
@@ -0,0 +1,45 @@
+<?php
+namespace Ibw\JobeetBundle\DataFixture\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Ibw\JobeetBundle\Entity\Category;
+
+class LoadCategoryData extends AbstractFixture implements
+    OrderedFixtureInterface
+{
+    public function load(ObjectManager $em)
+    {
+        $design = new Category();
+        $design->setName('Design');
+        $programming = new Category();
+        $programming->setName('Programming');
+        $manager = new Category();
+        $manager->setName('Manager');
+        $administrator = new Category();
+        $administrator->setName('Administrator');
+
+        $em->persist($design);
+        $em->persist($programming);
+        $em->persist($manager);
+        $em->persist($administrator);
+        $em->flush();
+
+        $this->addReference('category-design', $design);
+        $this->addReference('category-programming', $programming);
+        $this->addReference('category-manager', $manager);
+        $this->addReference('category-administrator', $administrator);
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    public function getOrder()
+    {
+        // TODO: Implement getOrder() method.
+        return 1;
+    }
+}
