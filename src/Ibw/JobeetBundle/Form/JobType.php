32ab36aeb7f27b8acf1c56fe56db224412e0807c
yboker <8236138@qq.com>
2015-03-19 18:09:41 +0800

    表单

--- src/Ibw/JobeetBundle/Form/JobType.php
+++ src/Ibw/JobeetBundle/Form/JobType.php
@@ -5,6 +5,7 @@ namespace Ibw\JobeetBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Ibw\JobeetBundle\Entity\Job;
 
 class JobType extends AbstractType
 {
