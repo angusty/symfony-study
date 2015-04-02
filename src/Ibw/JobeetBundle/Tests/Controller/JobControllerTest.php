6988b56242ea86f7b29f81b4084561b4ada7f33f
angusty <yboker1982@gmail.com>
2015-04-03 01:05:38 +0800

    添加test

--- src/Ibw/JobeetBundle/Tests/Controller/JobControllerTest.php
+++ src/Ibw/JobeetBundle/Tests/Controller/JobControllerTest.php
@@ -21,4 +21,176 @@ class JobControllerTest extends ControllerCase
         $max_jobs_on_homepage = $kernel->getContainer()->getParameter('max_jobs_on_homepage');
         $this->assertTrue($crawler->filter('.category_programming tr')->count() <= $max_jobs_on_homepage);
     }
+
+    public function testJobForm()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/job/new');
+        $this->assertEquals('Ibw\JobeetBundle\Controller\JobController::newAction', $client->getRequest()->attributes->get('_controller'));
+
+        $form = $crawler->selectButton('Preview your job')->form(array(
+            'job[company]' => 'Sensio Labs',
+            'job[url]' => 'http://angusty.com',
+            'job[file]' => __DIR__ . '/../../../../../web/bundles/ibwjobeet/images/sensio-labs.gif',
+            'job[position]' => 'Developer',
+            'job[location]' => '成都,四川',
+            'job[description]' => '你将使用symfony工作',
+            'job[how_to_apply]' => '给我发email',
+            'job[email]' => '8236138@qq.com',
+            'job[is_public]' => false
+        ));
+
+        $client->submit($form);
+        $this->assertEquals('Ibw\JobeetBundle\Controller\JobController::createAction', $client->getRequest()->attributes->get('_controller'));
+
+        $client->followRedirect();
+        $this->assertEquals('Ibw\JobeetBundle\Controller\JobController::previewAction', $client->getRequest()->attributes->get('_controller'));
+
+        //测试数据库记录
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+        $query = $em->createQuery('SELECT COUNT(j.id) FROM IbwJobeetBundle:Job j WHERE j.location=:location AND j.is_activated IS NULL AND j.is_public=0');
+        $query->setParameter('location', '成都,四川');
+        $this->assertTrue(0<$query->getSingleScalarResult());
+
+        //job信息表单测试
+        $crawler = $client->request('GET', '/job/new');
+        $form = $crawler->selectButton('Preview your job')->form(array(
+            'job[company]'      => 'Sensio Labs',
+            'job[position]'     => 'Developer',
+            'job[location]'     => 'Atlanta, USA',
+            'job[email]'        => 'not.an.email',
+        ));
+        $crawler = $client->submit($form);
+
+        // check if we have 3 errors
+        $this->assertTrue($crawler->filter('.error_list')->count() == 3);
+
+        // check if we have error on job_description field
+        $this->assertTrue($crawler->filter('#job_description')->siblings()->first()->filter('.error_list')->count() == 1);
+
+        // check if we have error on job_how_to_apply field
+        $this->assertTrue($crawler->filter('#job_how_to_apply')->siblings()->first()->filter('.error_list')->count() == 1);
+
+        // check if we have error on job_email field
+        $this->assertTrue($crawler->filter('#job_email')->siblings()->first()->filter('.error_list')->count() == 1);
+    }
+
+    public function testPublishJob()
+    {
+        $client = $this->createJob(array('job[position]' => 'FOO1'));
+        $crawler = $client->getCrawler();
+        $form = $crawler->selectButton('Publish')->form();
+        $client->submit($form);
+
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $query = $em->createQuery('SELECT count(j.id) from IbwJobeetBundle:Job j WHERE j.position = :position AND j.is_activated = 1');
+        $query->setParameter('position', 'FOO1');
+        $this->assertTrue(0 < $query->getSingleScalarResult());
+    }
+
+    public function testDeleteJob()
+    {
+        $client = $this->createJob(array('job[position]' => 'FOO2'));
+        $crawler = $client->getCrawler();
+        $form = $crawler->selectButton('Delete')->form();
+        $client->submit($form);
+
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $query = $em->createQuery('SELECT count(j.id) from IbwJobeetBundle:Job j WHERE j.position = :position');
+        $query->setParameter('position', 'FOO2');
+        $this->assertTrue(0 == $query->getSingleScalarResult());
+    }
+
+    public function createJob($values = array(), $publish = false)
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/job/new');
+        $form = $crawler->selectButton('Preview your job')->form(array_merge(array(
+            'job[company]'      => 'Sensio Labs',
+            'job[url]'          => 'http://www.sensio.com/',
+            'job[position]'     => 'Developer',
+            'job[location]'     => 'Atlanta, USA',
+            'job[description]'  => 'You will work with symfony to develop websites for our customers.',
+            'job[how_to_apply]' => 'Send me an email',
+            'job[email]'        => 'for.a.job@example.com',
+            'job[is_public]'    => false,
+        ), $values));
+
+        $client->submit($form);
+        $client->followRedirect();
+
+        if($publish) {
+            $crawler = $client->getCrawler();
+            $form = $crawler->selectButton('Publish')->form();
+            $client->submit($form);
+            $client->followRedirect();
+        }
+
+        return $client;
+    }
+
+    public function getJobByPosition($position)
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $query = $em->createQuery('SELECT j from IbwJobeetBundle:Job j WHERE j.position = :position');
+        $query->setParameter('position', $position);
+        $query->setMaxResults(1);
+        return $query->getSingleResult();
+    }
+
+    public function testEditJob()
+    {
+        $client = $this->createJob(array('job[position]' => 'FOO3'), true);
+        $crawler = $client->getCrawler();
+        $crawler = $client->request('GET', sprintf('/job/%s/edit', $this->getJobByPosition('FOO3')->getToken()));
+        $this->assertTrue(404 === $client->getResponse()->getStatusCode());
+    }
+
+    public function editAction($token)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('IbwJobeetBundle:Job')->findOneByToken($token);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Job entity.');
+        }
+
+        if ($entity->getIsActivated()) {
+            throw $this->createNotFoundException('Job is activated and cannot be edited.');
+        }
+
+        // ...
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
