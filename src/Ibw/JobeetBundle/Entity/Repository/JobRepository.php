22a9c09dcf872dfd8046ca4eff5a99050df21a99
angusty <yboker1982@gmail.com>
2015-04-08 00:52:49 +0800

    update

--- src/Ibw/JobeetBundle/Entity/Repository/JobRepository.php
+++ src/Ibw/JobeetBundle/Entity/Repository/JobRepository.php
@@ -69,6 +69,17 @@ class JobRepository extends EntityRepository
         $query = $qb->getQuery();
         return $query->getSingleScalarResult();
     }
+
+    public function cleanup($days)
+    {
+        $query = $this->createQueryBuilder('j')
+            ->delete()
+            ->where('j.is_activated IS NULL')
+            ->andWhere('j.created_at < :created_at')
+            ->setParameter('created_at',  date('Y-m-d', time() - 86400 * $days))
+            ->getQuery();
+        return $query->execute();
+    }
 }
 
 
