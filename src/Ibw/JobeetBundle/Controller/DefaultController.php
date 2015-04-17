e477ca9d918c147594ac715fe73368d9a459cd15
yboker <8236138@qq.com>
2015-04-17 16:17:35 +0800

    add

--- src/Ibw/JobeetBundle/Controller/DefaultController.php
+++ src/Ibw/JobeetBundle/Controller/DefaultController.php
@@ -10,4 +10,9 @@ class DefaultController extends Controller
     {
         return $this->render('IbwJobeetBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    public function loginAction()
+    {
+        return ;
+    }
 }
