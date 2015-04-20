bee89ffebd9ca254e1de684e73f52c9631b4d9a6
yboker <8236138@qq.com>
2015-04-20 13:45:19 +0800

    add

--- src/Ibw/JobeetBundle/Controller/DefaultController.php
+++ src/Ibw/JobeetBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Ibw\JobeetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,23 @@ class DefaultController extends Controller
 
     public function loginAction()
     {
-        return ;
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+        return $this->render('IbwJobeetBundle:Default:login.html.twig', array(
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error' => $error,
+        ));
+
     }
 }
+
+
+
+
